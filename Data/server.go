156c624420f7273eefec940de376f8ed0f0b9267
Ayoub Benramdane <ayoubbenramdane05@gmail.com>
2024-09-20 18:14:06 +0100

    done

--- Data/server.go
+++ Data/server.go
@@ -3,11 +3,15 @@ package Tracker
 import (
 	"html/template"
 	"net/http"
+	"strconv"
 )
 
 func Index(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.Error(w, "Status Not Found 404", http.StatusNotFound)
+		return
+	} else if r.Method != http.MethodGet {
+		http.Error(w, "Status Method Not Allowed 405", http.StatusMethodNotAllowed)
 		return
 	}
 	temp, err := template.ParseFiles("src/home.html")
@@ -15,11 +19,15 @@ func Index(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Status Internal Server Error 500", http.StatusInternalServerError)
 		return
 	}
-	temp.Execute(w, nil)
+	err = temp.Execute(w, Artists())
+	if err != nil {
+		http.Error(w, "Status Internal Server Error 500", http.StatusInternalServerError)
+		return
+	}
 }
 
 func Groupie(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
+	if r.Method != http.MethodGet {
 		http.Error(w, "Status Method Not Allowed 405", http.StatusMethodNotAllowed)
 		return
 	}
@@ -28,5 +36,14 @@ func Groupie(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Status Internal Server Error 500", http.StatusInternalServerError)
 		return
 	}
-	temp.Execute(w, nil)
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil || id < 1 || id > 52 {
+		http.Error(w, "Status Internal Server Error 500", http.StatusInternalServerError)
+		return
+	}
+	err = temp.Execute(w, Artist(id-1))
+	if err != nil {
+		http.Error(w, "Status Internal Server Error 500", http.StatusInternalServerError)
+		return
+	}
 }
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	Tracker "Tracker/Data"
@@ -8,5 +9,10 @@ import (
 
 func main() {
 	http.HandleFunc("/", Tracker.Index)
-	http.HandleFunc("/{{id}}", Tracker.Groupie)
+	http.HandleFunc("/artists/{id}", Tracker.Groupie)
+	fmt.Println("Server is Running... http://localhost:8404")
+	err := http.ListenAndServe(":8404", nil)
+	if err != nil {
+		fmt.Println("ListenAndServe: ", err)
+	}
 }
